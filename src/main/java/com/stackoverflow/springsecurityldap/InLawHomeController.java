d1669d07e8ab789e8dacb56ea04939ffa8c12ecc
Ajit <Ajit.kadam@live.in>
2018-01-14 02:12:17 +0530

    Added Class

---
+++ src/main/java/com/stackoverflow/springsecurityldap/InLawHomeController.java
@@ -0,0 +1,12 @@
+package com.stackoverflow.springsecurityldap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class InLawHomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(InLawHomeController.class);
+
+}
